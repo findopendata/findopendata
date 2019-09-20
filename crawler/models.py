599e45107ca7f8a87bdb717fd391b323e5a5f72c
ekzhu <ekzhu@users.noreply.github.com>
2019-09-20 03:26:39 -0400

    use eager loading for word vector model to save memory

--- crawler/models.py
+++ crawler/models.py
@@ -21,6 +21,22 @@ class _LazySpacyModel:
         return np.zeros(len(doc.vector), dtype=np.float32)
 
 
-WordVectorModel = _LazySpacyModel("en_vectors_web_lg")
+class _SpacyModel(_LazySpacyModel):
+    
+    def __init__(self, model_name, **kwargs):
+        super().__init__(model_name, **kwargs)
+        self._model = spacy.load(self._model_name, **self._model_kwargs)
+
 
 LanguageModel = _LazySpacyModel("en_core_web_sm")
+
+# When _LazySpacyModel is used, importing the model won't immediately load
+# the model, however every Celery worker process will load a redundant copy of
+# the model into its own memory -- high memory usage.
+# WordVectorModel = _LazySpacyModel("en_vectors_web_lg")
+
+# When _SpacyModel is used, the parent Celery process will load the model 
+# into its memory, and Celery workers (forked child processes) may not need
+# to have physical redundant copies due to "copy-on-write".
+WordVectorModel = _SpacyModel("en_vectors_web_lg")
+
