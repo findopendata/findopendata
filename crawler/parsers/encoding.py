890f59b58f4daa31563474e3997c4b1a5f3c91cb
ekzhu <ekzhu@users.noreply.github.com>
2019-10-22 22:48:08 -0700

    fix encoding detect

--- crawler/parsers/encoding.py
+++ crawler/parsers/encoding.py
@@ -9,7 +9,8 @@ magic_parser = magic.Magic(mime=True, mime_encoding=True)
 
 def guess_encoding_from_buffer(buf, chardet_threshold=0.5):
     result = chardet.detect(buf)
-    if result["confidence"] < chardet_threshold:
+    confidence = result.get("confidence")
+    if not confidence or confidence < chardet_threshold:
         # Try magic
         encoding = magic_encoding.from_buffer(buf)
         if not encoding.startswith("unknown"):
